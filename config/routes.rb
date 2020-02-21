2d3bdb2eaa19477a6257e8ff9a1665f6d272933b
Elisa Nogueira <elisanog05@gmail.com>
2020-02-21 12:21:57 -0300

    Created the dose delete feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 # Creating the routes
 Rails.application.routes.draw do
   resources :cocktails, only: %i[index show new create] do
-    resources :doses, only: %i[new create destroy]
+    resources :doses, only: %i[new create]
   end
+  resources :doses, only: 'destroy'
 end
